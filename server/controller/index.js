367f7f50753b855add89ef4d02236b334e644037
Julie Barwick <julie@juliebarwick.com>
2021-08-17 11:22:57 -0700

    Handle non-number input for getStyles

--- server/controller/index.js
+++ server/controller/index.js
@@ -12,6 +12,7 @@ module.exports = {
         res.sendStatus(404);
       });
   },
+
   getProductById: (req, res) => {
     const { id } = req.params;
     model.getProductInfo(id)
@@ -29,18 +30,26 @@ module.exports = {
 
   getStyles: (req, res) => {
     const { id } = req.params;
-    model.getProductStyles(id)
-      .then((data) => {
-        if (data.rows.length === 0) {
-          res.status(404).send('This product does not exist.');
-        } else {
-          res.send(data.rows[0]).status(200);
-        }
-      })
-      .catch((err) => {
-        console.error(err.stack);
-        res.sendStatus(404);
-      });
+    if (Number.isNaN(Number(id))) {
+      res.status(404).send('Error: invalid product id provided');
+    } else {
+      model.getProductStyles(id)
+        .then((data) => {
+          if (data.rows.length === 0) {
+            const nonExistentProduct = {
+              product_id: id,
+              results: data.rows,
+            };
+            res.send(nonExistentProduct).status(200);
+          } else {
+            res.send(data.rows[0]).status(200);
+          }
+        })
+        .catch((err) => {
+          console.error(err.stack);
+          res.sendStatus(404);
+        });
+    }
   },
 
   getRelated: (req, res) => {
