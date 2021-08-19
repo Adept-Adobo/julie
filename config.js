9c2e7d52c659b24255109028ea108a91cc64abc3
Julie Barwick <julie@juliebarwick.com>
2021-08-18 19:39:16 -0700

    Add config without keys - using env instead - and remove from gitignore

---
+++ config.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+};
